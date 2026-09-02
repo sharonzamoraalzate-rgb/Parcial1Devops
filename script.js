9ef99adbdfe4e27f53a8af3649c44696d6c9ae9d
JJAgudeloG <juanjoseagudelogonzalez@gmail.com>
2026-09-01 19:23:08 -0500

    boton nombres

---
+++ script.js
@@ -0,0 +1,5 @@
+// Función de tu compañero: mostrar nombres
+document.getElementById("btnNombres").addEventListener("click", function () {
+  document.getElementById("nombres").textContent = "Juan José Agudelo", "Sharon Zamora";
+});
+
